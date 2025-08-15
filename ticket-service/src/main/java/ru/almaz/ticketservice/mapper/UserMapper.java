8f01cf219ae54dd3cb125dbda6eb0b86cbad6ddd
Almazov-Artyom <artemalmazov3@gmail.com>
2025-08-16 02:44:08 +0300

    add UserMapper

---
+++ ticket-service/src/main/java/ru/almaz/ticketservice/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package ru.almaz.ticketservice.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import ru.almaz.ticketservice.dto.RegistrationRequest;
+import ru.almaz.ticketservice.entity.User;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserMapper {
+    User toUser(RegistrationRequest registrationRequest);
+}
