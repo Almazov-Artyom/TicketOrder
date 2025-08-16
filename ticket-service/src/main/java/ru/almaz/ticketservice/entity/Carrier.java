7a3924cb5cb6c6b68771ebf2902ccfbfae89e09d
Almazov-Artyom <artemalmazov3@gmail.com>
2025-08-16 20:39:36 +0300

    add Carrier

---
+++ ticket-service/src/main/java/ru/almaz/ticketservice/entity/Carrier.java
@@ -0,0 +1,12 @@
+package ru.almaz.ticketservice.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Carrier {
+    private Long id;
+
+    private String phoneNumber;
+}
