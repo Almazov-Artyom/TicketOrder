23be521c7f04e74db18238b9f4ff9a20fa0d92ea
Almazov-Artyom <artemalmazov3@gmail.com>
2025-08-16 18:05:56 +0300

    refactor RefreshTokenRequest

--- ticket-service/src/main/java/ru/almaz/ticketservice/dto/RefreshTokenRequest.java
+++ ticket-service/src/main/java/ru/almaz/ticketservice/dto/RefreshTokenRequest.java
@@ -1,9 +1,11 @@
 package ru.almaz.ticketservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 public record RefreshTokenRequest(
         @NotBlank(message = "RefreshToken не может быть пустым")
+        @JsonProperty("refresh_token")
         String refreshToken
 ) {
 }
