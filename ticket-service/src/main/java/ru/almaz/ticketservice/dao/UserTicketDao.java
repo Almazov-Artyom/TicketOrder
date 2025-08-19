c6121a29ef8200fc9b8df00eac40b7939a359ab9
Almazov-Artyom <artemalmazov3@gmail.com>
2025-08-20 02:10:58 +0300

    add findAllTicketsByUserId method in UserTicketDao

--- ticket-service/src/main/java/ru/almaz/ticketservice/dao/UserTicketDao.java
+++ ticket-service/src/main/java/ru/almaz/ticketservice/dao/UserTicketDao.java
@@ -4,12 +4,18 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ru.almaz.ticketservice.entity.Ticket;
+import ru.almaz.ticketservice.mapper.TicketRowMapper;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class UserTicketDao {
 
     private final JdbcTemplate jdbcTemplate;
+
+    private final TicketRowMapper ticketRowMapper;
 
     private static final String CREATE_TABLE_SQL = """
                 CREATE TABLE IF NOT EXISTS user_ticket (
@@ -24,6 +30,27 @@ public class UserTicketDao {
                 INSERT INTO user_ticket (user_id, ticket_id) VALUES (?, ?)
             """;
 
+    private static final String FIND_ALL_TICKETS_BY_USER_ID = """
+                SELECT
+                    t.id AS ticket_id,
+                    r.id AS route_id,
+                    r.origin AS route_origin,
+                    r.destination AS route_destination,
+                    c.id AS carrier_id,
+                    c.name AS carrier_name,
+                    c.phone_number AS carrier_phone_number,
+                    r.duration AS route_duration,
+                    t.departure_time AS ticket_departure_time,
+                    t.seat_number AS ticket_seat_number,
+                    t.price AS ticket_price,
+                    t.status AS ticket_status
+                FROM user_ticket AS ut
+                JOIN ticket AS t ON ut.ticket_id = t.id
+                JOIN route AS r ON t.route_id = r.id
+                JOIN carrier AS c ON r.carrier_id = c.id
+                WHERE ut.user_id = ?
+            """;
+
     @PostConstruct
     public void init() {
         jdbcTemplate.execute(CREATE_TABLE_SQL);
@@ -31,6 +58,10 @@ public class UserTicketDao {
 
     public void save(Long userId, Long ticketId) {
         jdbcTemplate.update(SAVE_SQL, userId, ticketId);
+    }
+
+    public List<Ticket> findAllTicketsByUserId(Long userId) {
+        return jdbcTemplate.query(FIND_ALL_TICKETS_BY_USER_ID, ticketRowMapper,userId);
     }
 
 
