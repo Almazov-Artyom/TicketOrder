cdb3dfe6889a64051ea42a33bb04cc323af416ec
Almazov-Artyom <artemalmazov3@gmail.com>
2025-08-19 19:01:10 +0300

    add getCurrentUserId method in UserService

--- ticket-service/src/main/java/ru/almaz/ticketservice/service/UserService.java
+++ ticket-service/src/main/java/ru/almaz/ticketservice/service/UserService.java
@@ -1,6 +1,7 @@
 package ru.almaz.ticketservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +24,11 @@ public class UserService {
     public User getUserByEmail(String email) {
         return userDao.findByEmail(email).
                 orElseThrow(()->new UserNotFoundException("Пользователь не найден"));
+    }
+
+    public Long getCurrentUserId() {
+        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return Long.valueOf(userId);
     }
 
     public UserDetailsService userDetailsService() {
