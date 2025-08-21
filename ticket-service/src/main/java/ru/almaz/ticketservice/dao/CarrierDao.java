b62b24163b4ecafd8843ff42f6fc01b87d31b2e8
Almazov-Artyom <artemalmazov3@gmail.com>
2025-08-21 19:20:19 +0300

    add save method in CarrierDao

--- ticket-service/src/main/java/ru/almaz/ticketservice/dao/CarrierDao.java
+++ ticket-service/src/main/java/ru/almaz/ticketservice/dao/CarrierDao.java
@@ -3,7 +3,13 @@ package ru.almaz.ticketservice.dao;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+import ru.almaz.ticketservice.entity.Carrier;
+
+import java.sql.PreparedStatement;
+import java.util.Objects;
+
 
 @Repository
 @RequiredArgsConstructor
@@ -18,8 +24,29 @@ public class CarrierDao {
                 )
             """;
 
+    private static final String SAVE_SQL= """
+                INSERT INTO carrier(name, phone_number)
+                VALUES (?, ?)
+                RETURNING id
+            """;
+
     @PostConstruct
     public void init(){
         jdbcTemplate.execute(CREATE_TABLE_SQL);
+    }
+
+    public Carrier save(Carrier carrier) {
+        GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(SAVE_SQL, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, carrier.getName());
+            ps.setString(2, carrier.getPhoneNumber());
+            return ps;
+        }, generatedKeyHolder);
+
+        carrier.setId((Long) generatedKeyHolder.getKey());
+
+        return carrier;
     }
 }
