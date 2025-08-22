8c9367fcbc1083453df2efff4e57083ac9ce3982
Almazov-Artyom <artemalmazov3@gmail.com>
2025-08-22 16:12:25 +0300

    add rest update carrier

--- ticket-service/src/main/java/ru/almaz/ticketservice/controller/CarrierController.java
+++ ticket-service/src/main/java/ru/almaz/ticketservice/controller/CarrierController.java
@@ -1,21 +1,28 @@
 package ru.almaz.ticketservice.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.almaz.ticketservice.dto.CarrierDto;
 import ru.almaz.ticketservice.dto.AddCarrierRequest;
+import ru.almaz.ticketservice.dto.UpdateCarrierDto;
 import ru.almaz.ticketservice.service.CarrierService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/carrier")
 public class CarrierController {
 
     private final CarrierService carrierService;
 
-    @PostMapping("/carrier")
-    public CarrierDto addCarrier(@RequestBody AddCarrierRequest carrierRequest) {
+    @PostMapping
+    public CarrierDto addCarrier(@RequestBody @Valid AddCarrierRequest carrierRequest) {
         return carrierService.save(carrierRequest);
+    }
+
+    @PatchMapping("/{carrierId}")
+    public CarrierDto updateCarrier(@PathVariable Long carrierId,
+                                    @RequestBody @Valid UpdateCarrierDto updateCarrierDto) {
+        return carrierService.updateCarrier(carrierId,updateCarrierDto);
     }
 }
--- ticket-service/src/main/java/ru/almaz/ticketservice/service/CarrierService.java
+++ ticket-service/src/main/java/ru/almaz/ticketservice/service/CarrierService.java
@@ -6,8 +6,10 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.almaz.ticketservice.dao.CarrierDao;
 import ru.almaz.ticketservice.dto.CarrierDto;
 import ru.almaz.ticketservice.dto.AddCarrierRequest;
+import ru.almaz.ticketservice.dto.UpdateCarrierDto;
 import ru.almaz.ticketservice.entity.Carrier;
 import ru.almaz.ticketservice.mapper.CarrierMapper;
+import ru.almaz.ticketservice.validator.CarrierValidator;
 
 @Service
 @RequiredArgsConstructor
@@ -17,10 +19,19 @@ public class CarrierService {
 
     private final CarrierMapper carrierMapper;
 
+    private final CarrierValidator carrierValidator;
+
     @Transactional
     public CarrierDto save(AddCarrierRequest carrierRequest) {
         Carrier carrier = carrierMapper.toCarrier(carrierRequest);
         carrierDao.save(carrier);
+        return carrierMapper.toCarrierDto(carrier);
+    }
+
+    @Transactional
+    public CarrierDto updateCarrier(Long carrierId,UpdateCarrierDto carrierRequest) {
+        carrierValidator.isCarrierValid(carrierId);
+        Carrier carrier = carrierDao.updateCarrier(carrierId, carrierRequest);
         return carrierMapper.toCarrierDto(carrier);
     }
 }
