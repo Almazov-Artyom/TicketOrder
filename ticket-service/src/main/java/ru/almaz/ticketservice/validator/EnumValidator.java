51804dbebbd45b26f102f11512c9730e2f5c7ac5
Almazov-Artyom <artemalmazov3@gmail.com>
2025-08-23 20:28:31 +0300

    add EnumValid annotation

---
+++ ticket-service/src/main/java/ru/almaz/ticketservice/validator/EnumValidator.java
@@ -0,0 +1,27 @@
+package ru.almaz.ticketservice.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import ru.almaz.ticketservice.annotation.EnumValid;
+import ru.almaz.ticketservice.enums.TicketStatus;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class EnumValidator implements ConstraintValidator<EnumValid, String> {
+
+    private Set<String> acceptedValues;
+
+    @Override
+    public void initialize(EnumValid annotation) {
+        acceptedValues = Arrays.stream(annotation.enumClass().getEnumConstants())
+                .map(Enum::name)
+                .collect(Collectors.toSet());
+    }
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        return value == null || acceptedValues.contains(value);
+
+    }
+}
