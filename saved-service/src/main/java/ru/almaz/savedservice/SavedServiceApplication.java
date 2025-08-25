d09d8da49dbde971a7010844cd226373d85609b8
Almazov-Artyom <artemalmazov3@gmail.com>
2025-08-25 04:24:59 +0300

    add module: saved-service

---
+++ saved-service/src/main/java/ru/almaz/savedservice/SavedServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.almaz.savedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavedServiceApplication.class, args);
+    }
+
+}
